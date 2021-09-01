115f492c8019e8ba0e30e09b93d051189a135372
viet34tqc <viet34tqc@gmail.com>
2021-09-01 10:45:12 +0700

    Update demo API Server

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: process.env.REACT_APP_API_SERVER,
+	baseURL: process.env.REACT_APP_API_SERVER || 'https://demo-usequery-server.herokuapp.com/',
 	headers: {
 		'Content-type': 'application/json',
 	},
-});
+})
 
 axiosInstance.interceptors.request.use(async (config) => {
 	return config;
